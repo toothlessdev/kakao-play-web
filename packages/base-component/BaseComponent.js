28fa4c4823da0e117e52bab315b49261e8126f19
toothlessdev <toothless042@gmail.com>
2025-05-02 03:12:55 +0900

    feature: BaseComponent 클래스 구현

---
+++ packages/base-component/BaseComponent.js
@@ -0,0 +1,51 @@
+import { State } from "./State.js";
+
+export class BaseComponent extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+        this.state = new State({}, () => this.reRender());
+    }
+
+    connectedCallback() {
+        this.onBeforeMount();
+        this.reRender();
+        this.onAfterMount();
+    }
+
+    disconnectedCallback() {
+        this.onUnmount();
+    }
+
+    onBeforeMount() {}
+
+    onAfterMount() {}
+
+    onUpdate() {}
+
+    onUnmount() {}
+
+    onEffect() {}
+
+    reRender() {
+        const renderedNode = this.render(this.state);
+
+        if (!(renderedNode instanceof Node)) {
+            throw new Error("render() 메서드는 html tagged template literal 또는 Node를 반환해야 합니다");
+        }
+
+        this.shadowRoot.innerHTML = "";
+        this.shadowRoot.appendChild(renderedNode);
+
+        this.onUpdate();
+        this.onEffect();
+    }
+
+    /**
+     * @param {Record<string,unknown>} state
+     * @returns {Node}
+     */
+    render(state) {
+        throw new Error("render() 메서드가 오버라이드되지 않았습니다");
+    }
+}
