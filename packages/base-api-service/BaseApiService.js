d6f12f9e32edf99830898b1966c4db5b7c0fc085
toothlessdev <toothless042@gmail.com>
2025-05-01 17:57:08 +0900

    feature: baseUrl, baseOptions 로 실제 요청 보내는 인스턴스를 싱글톤으로 관리하는 BaseApiService 구현

---
+++ packages/base-api-service/BaseApiService.js
@@ -0,0 +1,82 @@
+import { RequestHandler } from "./RequestHandler.js";
+
+export class BaseApiService {
+    static apiServiceInstance = null;
+    static baseUrl = "";
+    static baseOptions = {};
+
+    #requestHandler = null;
+
+    /**
+     * @param {String} baseUrl
+     * @param {RequestInit} baseOptions
+     */
+    constructor(baseUrl, baseOptions, requestHandler = new RequestHandler()) {
+        if (BaseApiService.apiServiceInstance) return BaseApiService.apiServiceInstance;
+
+        this.#requestHandler = requestHandler;
+        BaseApiService.baseUrl = baseUrl;
+        BaseApiService.baseOptions = baseOptions;
+        BaseApiService.apiServiceInstance = this;
+    }
+
+    /**
+     * @param {String} endPoint
+     * @param {RequestInit} options
+     * @returns {Promise<Response>}
+     */
+    async #request(endPoint = "", options = {}) {
+        const url = BaseApiService.baseUrl + endPoint;
+        const requestOptions = {
+            headers: { "Content-Type": "application/json" },
+            ...BaseApiService.baseOptions,
+            ...options,
+        };
+        return this.#requestHandler.handleRequest(url, requestOptions);
+    }
+
+    /**
+     * @param {String} endPoint
+     * @param {RequestInit} options
+     */
+    async get(endPoint = "", options = {}) {
+        return this.#request(endPoint, { method: "GET", ...options });
+    }
+
+    /**
+     * @param {String} endPoint
+     * @param {Record<string, any>} body
+     * @param {RequestInit} options
+     */
+    async post(endPoint = "", body = {}, options = {}) {
+        return this.#request(endPoint, {
+            method: "POST",
+            body: JSON.stringify(body),
+            ...options,
+        });
+    }
+
+    /**
+     * @param {String} endPoint
+     * @param {Record<string, any>} body
+     * @param {RequestInit} options
+     */
+    async put(endPoint = "", body = {}, options = {}) {
+        return this.#request(endPoint, {
+            method: "PUT",
+            body: JSON.stringify(body),
+            ...options,
+        });
+    }
+
+    /**
+     * @param {String} endPoint
+     * @param {RequestInit} options
+     */
+    async delete(endPoint = "", options = {}) {
+        return this.#request(endPoint, {
+            method: "DELETE",
+            ...options,
+        });
+    }
+}
