7648018645498ad31e9fd5be5aaea8576434c8ba
toothlessdev <toothless042@gmail.com>
2025-05-02 17:52:39 +0900

    test: BaseGlobalState 에서 전역상태 변경시 바인딩된 함수 호출에 대한 단위테스트

---
+++ packages/base-component/__test__/BaseGlobalState.test.js
@@ -0,0 +1,37 @@
+import { BaseGlobalState } from "../BaseGlobalState.js";
+import { test, expect } from "../../__test__/test.js";
+
+class GlobalState extends BaseGlobalState {
+    static instance = null;
+
+    constructor() {
+        if (GlobalState.instance) {
+            return GlobalState.instance;
+        }
+        super();
+        this.state.count = 0;
+        GlobalState.instance = this;
+    }
+}
+
+class TestComponent {
+    reRenderFunctionCalled = false;
+
+    constructor() {
+        this.globalState = new GlobalState();
+        this.globalState.subscribe(this, this.reRender.bind(this));
+    }
+
+    // 실제 BaseComponent extends 하는 경우 onUnMount 에서 구독해제 해야함!
+
+    reRender() {
+        this.reRenderFunctionCalled = true;
+    }
+}
+
+test("GlobalState 가 변경되면 구독한 컴포넌트가 바인딩한 함수가 호출됩니다", () => {
+    const testComponent = new TestComponent();
+    testComponent.globalState.state.count++;
+
+    expect(testComponent.reRenderFunctionCalled).toBe(true);
+});
