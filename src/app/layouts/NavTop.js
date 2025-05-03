fc696679702d41a6b520e1d2ada3fc589c9b1958
toothlessdev <toothless042@gmail.com>
2025-05-04 07:42:55 +0900

    feature: NavTop 컴포넌트 구현

---
+++ src/app/layouts/NavTop.js
@@ -0,0 +1,50 @@
+import { BaseComponent } from "../../../packages/base-component/BaseComponent.js";
+import { html } from "../../../packages/html-tagged-template-literal/HTMLTaggedTemplateLiteral.js";
+
+export class NavTop extends BaseComponent {
+    render() {
+        return html`
+            <style>
+                :host * {
+                    box-sizing: border-box;
+                }
+                nav {
+                    width: 100%;
+                    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+                }
+                ul {
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-between;z
+
+                    margin: 0;
+                    max-width: 1000px;
+                    height: 60px;
+
+                    margin: 0 auto;
+                    padding: 0px 20px;
+                }
+                li {
+                    margin: 0;
+                    list-style: none;
+                }
+                li:first-child {
+                    font-weight: 400;
+                    font-size: 20px;
+                }
+            </style>
+
+            <nav>
+                <ul>
+                    <li>Kakao Play</li>
+                    <li>
+                        <movie-search-bar></movie-search-bar>
+                    </li>
+                    ${html``}
+                </ul>
+            </nav>
+        `;
+    }
+}
+
+customElements.define("nav-top", NavTop);
