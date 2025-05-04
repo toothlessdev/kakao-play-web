95b8cdeebaa1cf679d19e92b1133c06633b0cbc0
toothlessdev <toothless042@gmail.com>
2025-05-05 02:47:50 +0900

    refactor: BasePersistGlobalState에서 BasePersistStorage 사용

--- packages/base-component/BasePersistGlobalState.js
+++ packages/base-component/BasePersistGlobalState.js
@@ -1,20 +1,26 @@
 import { BaseJsonStorageService } from "../base-json-storage-service/BaseJsonStorageService.js";
 import { BaseGlobalState } from "./BaseGlobalState.js";
 
+class BasePersistStorage extends BaseJsonStorageService {
+    constructor(storageKey, localStorage) {
+        super(`persist/${storageKey}`, localStorage);
+    }
+}
+
 export class BasePersistGlobalState extends BaseGlobalState {
     /**
      * @param {String} storageKey
      * @param {window.localStorage} localStorage
      */
     constructor(storageKey, initialState, localStorage = window.localStorage) {
-        const jsonStorageService = new BaseJsonStorageService(storageKey, localStorage);
-        const persisted = jsonStorageService.getData();
+        const basePersistStorage = new BasePersistStorage(storageKey, localStorage);
+        const persisted = basePersistStorage.getData();
 
         super(persisted || initialState);
-        this.jsonStorageService = jsonStorageService;
+        this.basePersistStorage = basePersistStorage;
     }
 
     onDispatch() {
-        this.jsonStorageService.setData(this.state);
+        this.basePersistStorage.setData(this.state);
     }
 }
